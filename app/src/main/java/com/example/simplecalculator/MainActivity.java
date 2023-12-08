b1426fe1683e3dc926d16d7b2f2efbb73e66c978
ErelWeinstein <erelweinstein123@gmail.com>
2023-12-08 10:45:58 +0200

    4 new buttons: btnPlus,btnMinus,btnMult, btnDiv

--- app/src/main/java/com/example/simplecalculator/MainActivity.java
+++ app/src/main/java/com/example/simplecalculator/MainActivity.java
@@ -3,12 +3,16 @@ package com.example.simplecalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
+    @Overrides
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onBtnClicked(View view) {
     }
 }
